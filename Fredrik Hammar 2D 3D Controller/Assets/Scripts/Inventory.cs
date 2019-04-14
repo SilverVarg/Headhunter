798c2bb279e2cb3c65240ffb819007143e256721
jh658 <32582127+jh658@users.noreply.github.com>
2019-04-15 00:27:12 +0200

    Börjat på ett inventarium.
    Har gjort delar  till ett inventarium.

---
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/Inventory.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private bool inventoryEnabled;
+    public GameObject inventory;
+
+    private int allSlots;
+    private int enabledSlots;
+    private GameObject[] slot;
+
+    public GameObject slotHolder;
+
+    void Start()
+    {
+        allSlots = 2;
+        slot = new GameObject[allSlots];
+
+        for (int i = 0; i < allSlots; i++)
+        {
+            slot[i] = slotHolder.transform.GetChild(i).gameObject;
+
+            // Checks if slot has no gameobject. If so it sets slot to true.
+            if (slot[i].GetComponent<Slot>().item == null)
+                slot[i].GetComponent<Slot>().empty = true;
+
+        }
+    }
+
+    // Genom att trycka knapp "i" får man fram och döljer inventariet.
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.I))
+            inventoryEnabled = !inventoryEnabled;
+
+        if (inventoryEnabled == true)
+        {
+            inventory.SetActive(true);
+        } else {
+            inventory.SetActive(false);
+        }
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Item")
+        {
+            GameObject itemPickedUp = other.gameObject;
+            Item item = itemPickedUp.GetComponent<Item>();
+
+            AddItem(itemPickedUp, item.ID, item.type, item.description, item.icon);
+        }
+
+    }
+
+    // Add items to inventory. Checking for empty slots, 
+    void AddItem(GameObject itemObject, int itemID, string itemType, string itemDescription, Sprite itemIcon)
+    {
+        for (int i = 0; i < allSlots; i++)
+        {
+            if(slot[i].GetComponent<Slot>().empty)
+            {
+                // Add item to slot
+                itemObject.GetComponent<Item>().pickedUp = true;
+
+                slot[i].GetComponent<Slot>().item = itemObject;
+                slot[i].GetComponent<Slot>().icon = itemIcon;
+                slot[i].GetComponent<Slot>().ID = itemID;
+                slot[i].GetComponent<Slot>().type = itemType;
+                slot[i].GetComponent<Slot>().description = itemDescription;
+
+                itemObject.transform.parent = slot[i].transform;
+                itemObject.SetActive(false);
+
+                slot[i].GetComponent<Slot>().UpdateSlot();
+                slot[i].GetComponent<Slot>().empty = false;
+
+            }
+            return;
+        }
+
+    }
+
+}
