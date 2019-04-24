6a5f5529fa30c51eb50ce1c788f45ea5569633dc
jh658 <32582127+jh658@users.noreply.github.com>
2019-04-25 01:40:36 +0200

    Healthbar
    Har skapat en fungerande healthbar. Den tar skada av EnemyBall.

---
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController gameControllerInstance;
+    public Slider healthSlider;
+    public float playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameControllerInstance = this;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.value = playerHealth;
+    }
+}
---
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public float health = 100;
+    private float damageTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        damageTimer += Time.deltaTime;
+        GameController.gameControllerInstance.playerHealth = health;
+    }
+
+    public void Harm(float dmg)
+    {
+        if (damageTimer > 1.0f)
+        {
+            health -= dmg;
+            damageTimer = 0;
+        }
+
+
+    }
+}
