2f753d537bac3e424a285139439c52b4b064cf09
TeaLund <37661322+TeaLund@users.noreply.github.com>
2019-04-24 12:07:09 +0200

    tutorial scrolls

---
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/TutorialScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScript : MonoBehaviour
+{
+    public GameObject scroll;
+    public GameObject text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scroll.SetActive(false);
+        text.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter trigger");
+
+        if (other.CompareTag("Player"))
+        {
+            scroll.SetActive(true);
+            text.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            scroll.SetActive(false);
+            text.SetActive(false);
+        }
+    }
+}
