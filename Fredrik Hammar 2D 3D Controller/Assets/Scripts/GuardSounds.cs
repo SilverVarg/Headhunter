324d1fd22c677927e1eee3de3f92f65d41236c35
TeaLund <37661322+TeaLund@users.noreply.github.com>
2019-04-25 17:57:41 +0200

    moved scripts to scripts folder + added guard noises

---
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/GuardSounds.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardSounds : MonoBehaviour
+{
+    private AudioSource source;
+    public AudioClip clip;
+    private float range;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        range = Random.Range(0.5f, 1.5f);
+
+        if (!source.isPlaying)
+        {
+            source.pitch = range;
+            source.PlayOneShot(clip);
+        }
+        
+    }
+}
