e154daab7a90de61fed75e75a299cecf3bfd55b4
jh658 <32582127+jh658@users.noreply.github.com>
2019-04-29 15:08:17 +0200

    PickUp och Inventory
    Försökte få till pickup till inventoryt

--- Fredrik Hammar 2D 3D Controller/Assets/Scripts/InventoryItem.cs
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/InventoryItem.cs
@@ -1,20 +1,29 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class InventoryItem : MonoBehaviour
-//{
-//    public enum ITEMTYPE {BALL, BALL2, BALL3};
-//    public ITEMTYPE Type;
-//    public Sprite GUI_Icon = null;
+public class InventoryItem : MonoBehaviour
+{
+    public enum ITEMTYPE { BALL, BALL2, BALL3 };
+    public ITEMTYPE Type;
+    public Sprite GUI_Icon = null;
 
-//    void OnTriggerEnter(Collider Col)
-//    {
-//        if (!Col.CompareTag("Player"))
-//            return;
+    void Update()
+    {
+        if (CompareTag("InventoryItem") && Input.GetKeyDown(KeyCode.E))
+        {
+            //CheckPickUp(GetComponent<Collider>());
 
-//        // Add this item to the inventory
-//        Inventory.AddItem(gameObject);
-//    }
+            Inventory.AddItem(gameObject);
+        }
+    }
 
-//}
+    //void CheckPickUp(Collider Col)
+    //{
+    //    if (!Col.CompareTag("Player"))
+    //        return;
+
+    //    Inventory.AddItem(gameObject);
+    //}
+
+}
