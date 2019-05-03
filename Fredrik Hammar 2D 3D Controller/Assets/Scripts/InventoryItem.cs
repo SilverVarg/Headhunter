5ee3380b059036b91f68e76fc1b63eea19192917
jh658 <32582127+jh658@users.noreply.github.com>
2019-05-03 16:15:13 +0200

    Gjorde om inventoryt
    Gjorde om inventoryt från scratch

--- Fredrik Hammar 2D 3D Controller/Assets/Scripts/InventoryItem.cs
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/InventoryItem.cs
@@ -6,48 +6,53 @@ using UnityEngine;
 public class InventoryItem : MonoBehaviour
 {
 
-    //public enum ITEMTYPE { BALL, BALL2, BALL3 };
-    //public ITEMTYPE Type;
+    public enum ITEMTYPE { OILCAN, POWERUP, SALTBOTTLE };
+    public ITEMTYPE Type;
     public Sprite GUI_Icon = null;
 
-    //void OnTriggerEnter(Collider Col)
+    void OnTriggerEnter(Collider Col)
+    {
+        if (!Col.CompareTag("Player"))
+            return;
+
+        
+       // Add this item to the inventory
+        Inventory.AddItem(gameObject);
+        Debug.Log("Added object");
+        
+    }
+
+
+
+
+    void Start()
+    {
+
+
+    }
+
+
+    //void Update(Collider Col)
     //{
-    //    if (!Col.CompareTag("Player"))
+    //    if (Input.GetKeyDown(KeyCode.E) && !Col.CompareTag("Player") && )
     //        return;
 
     //    // Add this item to the inventory
     //    Inventory.AddItem(gameObject);
+
+    //if (Input.GetKeyDown(KeyCode.E))
+    //{
+    //    if (CompareTag("Saltbottle") || CompareTag("Powerup") || CompareTag("Oilcan") && (player.transform.position - this.transform.position).sqrMagnitude < 3 * 3)
+    //    {
+    //        //CheckPickUp(GetComponent<Collider>());
+    //        if (transform.parent = null)
+    //        {
+    //            Inventory.AddItem(gameObject);
+    //        }
+
+    //    }
     //}
 
-
-    //public enum ITEMTYPE { BALL };
-    //public ITEMTYPE Type;
-    //public Sprite GUI_Icon = null;
-    private GameObject player;
-    //private Inventory thisInventory;
-
-    void Start()
-    {
-        player = GameObject.Find("Player");
-
-    }
-
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            if (CompareTag("Saltbottle") || CompareTag("Powerup") || CompareTag("Oilcan") && (player.transform.position - this.transform.position).sqrMagnitude < 3 * 3)
-            {
-                //CheckPickUp(GetComponent<Collider>());
-                if (transform.parent = null)
-                {
-                    Inventory.AddItem(gameObject);
-                }
-
-            }
-        }
-    }
 
     //void CheckPickUp(Collider Col)
     //{
