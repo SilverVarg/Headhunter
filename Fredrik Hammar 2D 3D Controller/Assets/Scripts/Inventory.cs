9f1f40202217745ba33e7d87062defa25c95f49f
jh658 <32582127+jh658@users.noreply.github.com>
2019-05-04 01:42:51 +0200

    Inventory
    Eländet plågar oss vidare.

--- Fredrik Hammar 2D 3D Controller/Assets/Scripts/Inventory.cs
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/Inventory.cs
@@ -5,24 +5,23 @@ using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    protected int objectsInInventory = 0;
+    //protected int objectsInInventory = 0;
     
-    public void CountInventory()
-    {
-        objectsInInventory++;
-    }
+    //public void CountInventory()
+    //{
+    //    objectsInInventory++;
+    //}
 
-    public void removeOneFromInventory()
-    {
-        objectsInInventory--;
-    }
+    //public void removeOneFromInventory()
+    //{
+    //    objectsInInventory--;
+    //}
 
-    public int GetAmountOfObjectsInInventory()
-    {
-        return objectsInInventory;
-    }
+    //public int GetAmountOfObjectsInInventory()
+    //{
+    //    return objectsInInventory;
+    //}
     
-
     // Property for maintaining single instance
     public static Inventory Instance
     {
@@ -35,10 +34,7 @@ public class Inventory : MonoBehaviour
             }
             return ThisInstance;
         }
-
     }
-
-
 
     // Reference to singleton object
     private static Inventory ThisInstance = null;
@@ -61,29 +57,8 @@ public class Inventory : MonoBehaviour
     }
 
 
-
-
-    public static void Start()
-    {
-
-        
-
-    }
-
-    public static void Update()
-    {
-        //if (Input.GetKeyDown(KeyCode.I))
-        //{
-
-        //    ThisInstance.enabled = !ThisInstance.enabled;
-        //    Debug.Log("Knappen i trycktes ned");
-        //}
-    }
-
-
  
     public static void AddItem(GameObject GO)
-
         {
         
             // Disable all colliders for added object
@@ -95,7 +70,7 @@ public class Inventory : MonoBehaviour
                 MR.enabled = false;
 
             // Add to first available slot
-            for (int i = 0; i < ThisInstance.ItemList.childCount; i++)
+            for (int i=0; i<ThisInstance.ItemList.childCount; i++)
             {
                 Transform Item = ThisInstance.ItemList.GetChild(i);
 
@@ -110,7 +85,7 @@ public class Inventory : MonoBehaviour
         
         }
 
-    
+
 
 
     //public static void RemoveItem()
@@ -140,138 +115,3 @@ public class Inventory : MonoBehaviour
     //    }
     //}
 }
-
-
-
-
-
-
-    //using System.Collections;
-    //using System.Collections.Generic;
-    //using UnityEngine;
-    //using UnityEngine.UI;
-
-    //public class Inventory : MonoBehaviour
-    //{
-    //    protected int objectsInInventory = 0;
-    //    public void CountInventory()
-    //    {
-    //        objectsInInventory++;
-    //    }
-
-    //    public void removeOneFromInventory()
-    //    {
-    //        objectsInInventory--;
-    //    }
-
-    //    public int GetAmountOfObjectsInInventory()
-    //    {
-    //        return objectsInInventory;
-    //    }
-    //    // Property for maintaining single instance
-    //    public static Inventory Instance
-    //    {
-    //        get
-    //        {
-    //            if (ThisInstance == null)
-    //            {
-    //                GameObject InventoryObject = new GameObject("Inventory");
-    //                ThisInstance = InventoryObject.AddComponent<Inventory>();
-    //            }
-    //            return ThisInstance;
-    //        }
-
-    //    }
-
-
-
-    //    // Reference to singleton object
-    //    private static Inventory ThisInstance = null;
-
-    //    // Root object of item list
-    //    public RectTransform ItemList = null;
-    //    //----------------------------------------------------
-    //    // Use this for initialization
-    //    private void Awake()
-    //    {
-    //        // If single object already exists then destroy
-    //        if (ThisInstance != null)
-    //        {
-    //            DestroyImmediate(gameObject);
-    //            return;
-    //        }
-
-    //        // Make this single instance
-    //        ThisInstance = this;
-    //    }
-
-
-
-    //    public static void Start()
-    //    {
-
-    //        //ThisInstance.GetComponent<Inventory>();
-
-    //    }
-
-    //    public static void Update()
-    //    {
-    //        //if (Input.GetKeyDown(KeyCode.I))
-    //        //{
-
-    //        //    ThisInstance.enabled = !ThisInstance.enabled;
-    //        //    Debug.Log("Knappen i trycktes ned");
-    //        //}
-    //    }
-
-
-    //    //----------------------------------------------------
-    //    public static void AddItem(GameObject GO)
-    //    {
-    //        {
-    //            //Disable all colliders for added object
-    //            foreach (Collider C in GO.GetComponents<Collider>())
-    //                C.enabled = false;
-
-    //            // Disable renderers
-    //            foreach (MeshRenderer MR in GO.GetComponents<MeshRenderer>())
-    //                MR.enabled = false;
-
-    //            // Add to first available slot
-    //            for (int i = 0; i < ThisInstance.ItemList.childCount; i++)
-    //            {
-    //                Transform Item = ThisInstance.ItemList.GetChild(i);
-    //                // If item is not active, then it becomes new slot
-    //                if (!Item.gameObject.activeSelf)
-    //                {
-    //                    Item.GetComponent<Image>().sprite = GO.GetComponent<PickUp>().GUI_Icon;
-    //                    Item.gameObject.SetActive(true);
-    //                    return;
-    //                }
-    //            }
-    //        }
-
-    //    }
-
-
-    //    public static void RemoveItem()
-    //    { if (Input.GetKeyDown(KeyCode.U))
-    //        {
-    //            Transform itemToRemove = ThisInstance.ItemList.GetChild(0);
-    //            GameObject GO = itemToRemove.gameObject;
-
-    //            foreach (Collider C in GO.GetComponents<Collider>())
-    //                C.enabled = true;
-
-    //            foreach (MeshRenderer MR in GO.GetComponents<MeshRenderer>())
-    //                MR.enabled = true;
-
-    //            GO.SetActive(true);
-
-    //            GameObject iconToRemove = GameObject.FindGameObjectWithTag("ItemSlot00");
-    //            iconToRemove.GetComponent<SpriteRenderer>().enabled = false;
-    //            Inventory.Instance.removeOneFromInventory();
-    //        }
-    //    }
-    //}
-
--- Fredrik Hammar 2D 3D Controller/Assets/Scripts/InventoryItem.cs
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/InventoryItem.cs
@@ -2,11 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class InventoryItem : MonoBehaviour
 {
 
-    public enum ITEMTYPE { OILCAN, POWERUP, SALTBOTTLE };
+    public enum ITEMTYPE { InventoryItem };
     public ITEMTYPE Type;
     public Sprite GUI_Icon = null;
 
@@ -15,42 +14,36 @@ public class InventoryItem : MonoBehaviour
         if (!Col.CompareTag("Player"))
             return;
 
-        
-       // Add this item to the inventory
+        // Add this item to the inventory
         Inventory.AddItem(gameObject);
         Debug.Log("Added object");
-        
-    }
-
-
-
-
-    void Start()
-    {
-
 
     }
+
+
+
+
 
 
     //void Update(Collider Col)
     //{
-    //    if (Input.GetKeyDown(KeyCode.E) && !Col.CompareTag("Player") && )
+    //    if (Input.GetKeyDown(KeyCode.E) && !Col.CompareTag("Player"))
     //        return;
 
     //    // Add this item to the inventory
     //    Inventory.AddItem(gameObject);
 
-    //if (Input.GetKeyDown(KeyCode.E))
-    //{
-    //    if (CompareTag("Saltbottle") || CompareTag("Powerup") || CompareTag("Oilcan") && (player.transform.position - this.transform.position).sqrMagnitude < 3 * 3)
-    //    {
-    //        //CheckPickUp(GetComponent<Collider>());
-    //        if (transform.parent = null)
-    //        {
-    //            Inventory.AddItem(gameObject);
-    //        }
+    //    //if (Input.GetKeyDown(KeyCode.E))
+    //    //{
+    //    //    if (CompareTag("Saltbottle") || CompareTag("Powerup") || CompareTag("Oilcan") && (player.transform.position - this.transform.position).sqrMagnitude < 3 * 3)
+    //    //    {
+    //    //        //CheckPickUp(GetComponent<Collider>());
+    //    //        if (transform.parent = null)
+    //    //        {
+    //    //            Inventory.AddItem(gameObject);
+    //    //        }
 
-    //    }
+    //    //    }
     //}
 
 
