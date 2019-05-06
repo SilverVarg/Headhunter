e23af5d0bb46f5496fbcf570d0f59fd8ea02968e
TeaLund <37661322+TeaLund@users.noreply.github.com>
2019-05-06 17:51:54 +0200

    Added main menu scene + started on script for menu handling

---
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject optionsScreen;
+    public GameObject creditsScreen;
+
+    private void Start()
+    {
+        optionsScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+    }
+
+    public void ContinueGame()
+    {
+        //load latest checkpoint
+    }
+
+    public void StartGame()
+    {
+        Debug.Log("Click button");
+        SceneManager.LoadScene(1);
+    }
+
+    public void OptionsScreen()
+    {
+        optionsScreen.SetActive(true);
+    }
+
+    public void CreditsScreen()
+    {
+        creditsScreen.SetActive(true);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
