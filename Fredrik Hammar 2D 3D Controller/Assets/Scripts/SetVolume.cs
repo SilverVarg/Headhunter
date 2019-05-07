174d5ee9ce3c35eed855bca2b476c813dfc2634b
TeaLund <37661322+TeaLund@users.noreply.github.com>
2019-05-07 14:24:14 +0200

    fixed so music volume stays between levels

--- Fredrik Hammar 2D 3D Controller/Assets/Scripts/SetVolume.cs
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/SetVolume.cs
@@ -7,8 +7,14 @@ public class SetVolume : MonoBehaviour
 {
     public AudioMixer mixer;
 
+    private void Update()
+    {
+        mixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("Volume"));
+    }
+
     public void SetLevel(float SliderValue)
     {
         mixer.SetFloat("MusicVol", Mathf.Log10(SliderValue) * 20);
+        PlayerPrefs.SetFloat("Volume", Mathf.Log10(SliderValue) * 20);
     }    
 }
