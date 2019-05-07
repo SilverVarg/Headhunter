26432aa7d345450c68c76cefdec83871558affe5
TeaLund <37661322+TeaLund@users.noreply.github.com>
2019-05-07 12:12:58 +0200

    Main menu wip

--- Fredrik Hammar 2D 3D Controller/Assets/Scripts/MenuController.cs
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/MenuController.cs
@@ -7,11 +7,33 @@ public class MenuController : MonoBehaviour
 {
     public GameObject optionsScreen;
     public GameObject creditsScreen;
+    public GameObject controlsScreen;
+    public GameObject continueButton;
 
     private void Start()
     {
         optionsScreen.SetActive(false);
         creditsScreen.SetActive(false);
+        controlsScreen.SetActive(false);
+        continueButton.SetActive(false);
+
+        //Section below should be activated after a checkpoint system has been created
+        //if (Player has a checkpoint)
+        //{
+        //    continueButton.SetActive(true);
+        //}
+        //else
+        //{
+        //    continueButton.SetActive(false);
+        //}
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToMain();
+        }
     }
 
     public void ContinueGame()
@@ -20,14 +42,18 @@ public class MenuController : MonoBehaviour
     }
 
     public void StartGame()
-    {
-        Debug.Log("Click button");
+    {        
         SceneManager.LoadScene(1);
     }
 
     public void OptionsScreen()
     {
         optionsScreen.SetActive(true);
+    }
+
+    public void ControlsScreen()
+    {
+        controlsScreen.SetActive(true);
     }
 
     public void CreditsScreen()
@@ -38,5 +64,18 @@ public class MenuController : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void HideMenues()
+    {
+        creditsScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        controlsScreen.SetActive(false);
+    }
+
+    //function temporarily replacing a pause function, for the sake of testing
+    public void ReturnToMain()
+    {
+        SceneManager.LoadScene(0);
     }
 }
