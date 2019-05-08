eb337277adc4db1c523b59c5add45f4fad565dbc
jh658 <32582127+jh658@users.noreply.github.com>
2019-05-08 14:06:33 +0200

    Update healthshit
    Healthbar och bloodsplatter färdigt.

--- Fredrik Hammar 2D 3D Controller/Assets/Scripts/GameController.cs
+++ Fredrik Hammar 2D 3D Controller/Assets/Scripts/GameController.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class GameController : MonoBehaviour
 {
     public static GameController gameControllerInstance;
+    
     public Slider healthSlider;
     public float playerHealth;
+    
 
     // Start is called before the first frame update
     void Start()
